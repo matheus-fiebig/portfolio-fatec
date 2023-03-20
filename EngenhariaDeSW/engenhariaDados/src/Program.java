c796571ea4459462fbd815a9aa43ea4f63f314b6
matheus-fiebig <61194755+matheus-fiebig@users.noreply.github.com>
2023-03-20 19:12:56 -0300

    test + code

---
+++ EngenhariaDeSW/engenhariaDados/src/Program.java
@@ -0,0 +1,8 @@
+package src;
+public class Program {
+
+    public static void main (String[] args){
+
+        
+    }
+}
---
+++ EngenhariaDeSW/engenhariaDados/src/tests/carrinhoTest.java
@@ -0,0 +1,55 @@
+package src.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import src.carrinho.CarrinhoDeCompra;
+import src.carrinho.Categoria;
+import src.carrinho.Produto;
+
+public class carrinhoTest {
+    private final CarrinhoDeCompra carrinhoCompra;
+    public carrinhoTest() {
+        carrinhoCompra = new CarrinhoDeCompra();
+    }
+
+    @Test
+    public void getTotalDeveriaRetornarASomaDeTodosProdutos(){
+        //Arrange
+        carrinhoCompra.adicionarProduto(new Produto("P1", 1, new Categoria("Teste")));
+        carrinhoCompra.adicionarProduto(new Produto("P2", 2, new Categoria("Teste")));
+        carrinhoCompra.adicionarProduto(new Produto("P3", 3, new Categoria("Teste")));
+        
+        //Act
+        var total = carrinhoCompra.getTotal();
+
+        //Assert
+        assertTrue(total == 6);
+    }
+
+    @Test
+    public void adicionarProdutoDeveAdicionarUmUnicoProdutoALista(){
+        //Arrange & Act
+        carrinhoCompra.adicionarProduto(new Produto("P2", 2, new Categoria("Teste")));
+        carrinhoCompra.adicionarProduto(new Produto("P3", 3, new Categoria("Teste")));
+
+        //Assert
+        assertTrue(carrinhoCompra.getCarrinho().size() == 2);
+    }
+
+    @Test
+    public void finalizarCompraDeveLimparCarrinho(){
+        //Arrange
+        carrinhoCompra.adicionarProduto(new Produto("P2", 2, new Categoria("Teste")));
+        carrinhoCompra.adicionarProduto(new Produto("P3", 3, new Categoria("Teste")));
+
+        //Act
+        carrinhoCompra.finalizarCompra();
+        
+        //Assert
+        assertTrue(carrinhoCompra.getCarrinho().isEmpty());
+    }
+}
