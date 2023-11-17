332f5577194a2014cf2aae447124d354feac574c
Matheus Fiebig <macrubig@gmail.com>
2023-11-17 20:32:30 -0300

    Bertoti - 3sem API

---
+++ fruit/bertoti_fruta/src/main/java/bertoti/fruta/bertoti_fruta/BertotiFrutaApplication.java
@@ -0,0 +1,13 @@
+package bertoti.fruta.bertoti_fruta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BertotiFrutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BertotiFrutaApplication.class, args);
+	}
+
+}
---
+++ fruit/bertoti_fruta/src/test/java/bertoti/fruta/bertoti_fruta/BertotiFrutaApplicationTests.java
@@ -0,0 +1,13 @@
+package bertoti.fruta.bertoti_fruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BertotiFrutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
